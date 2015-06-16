3595c27ba97e547dd09c839fa406295936a82a1a
ZhuangXiaobin <zhuangxiaobin@joyschool.cn>
2015-06-16 21:57:36 +0800

    soluation1, 2

---
+++ src/ListNode.java
@@ -0,0 +1,8 @@
+/**
+ * Created by zxb on 15/6/16.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
---
+++ src/Solution2.java
@@ -0,0 +1,33 @@
+/**
+ * Add Two Numbers
+ * You are given two linked lists representing two non-negative numbers.
+ * The digits are stored in reverse order and each of their nodes contain a single digit.
+ * Add the two numbers and return it as a linked list.
+ * Input: 2 -> 4 -> 3
+ *        5 -> 6 -> 4
+ * Output: 7 -> 0 -> 8
+ */
+public class Solution2 {
+
+    public final static void main(String[] args) {
+        ListNode node1 = new ListNode(2);
+        node1.next = new ListNode(4);
+        node1.next.next = new ListNode(3);
+
+        ListNode node2 = new ListNode(5);
+        node2.next = new ListNode(6);
+        node2.next.next = new ListNode(4);
+
+        ListNode result = new Solution2().addTwoNumbers(node1, node2);
+        do {
+            System.out.print(result.val + "->");
+        } while(result.next != null);
+    }
+
+    private ListNode addTwoNumbers(ListNode node1, ListNode node2) {
+        ListNode result = null;
+
+        return result;
+    }
+
+}
